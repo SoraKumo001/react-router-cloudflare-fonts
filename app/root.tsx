68252c7e6ff2584258c1431c4eb29707ca2482a9
SoraKumo <info@croud.jp>
2025-02-17 10:05:15 +0900

    refactor: Remove unused code, update CloudflareFonts import and add dependency

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,7 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import { CloudflareFonts } from "./libs/CloudflareFonts";
+import { CloudflareFonts } from "cloudflare-fonts";
 import "./app.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
